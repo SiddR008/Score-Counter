c6e5cfdf878b7a4061e6a22c5f688863cefd3c7d
SIdd <107976187+SiddR008@users.noreply.github.com>
2024-03-07 22:59:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let addOne = document.getElementById("score-home")
+
+let count = 0
+
+
+function increment1() {
+    count += 1
+    addOne.textContent = count
+}
+
+function increment3() {
+    count += 3
+    addOne.textContent = count
+}
+
+function increment5() {
+    count += 5
+    addOne.textContent = count
+}
+
+let addGuest = document.getElementById("score-guest")
+let add = 0
+
+function increment1guest() {
+    add += 1
+    addGuest.textContent = add
+}
+
+function increment3guest() {
+    add += 3
+    addGuest.textContent = add
+}
+
+function increment5guest() {
+    add += 5
+    addGuest.textContent = add
+}
